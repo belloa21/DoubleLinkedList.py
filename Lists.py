35b97e13c52a350d44b0a9c1449a9caf8044f813
alexr <belloa@hartwick.edu>
2020-03-04 10:41:46 -0500

    Started Lab

--- Lists.py
+++ Lists.py
@@ -4,7 +4,7 @@
 
 
 class New_Node:
-    def __init__(self, data=none, prev=none, next=none):
+    def __init__(self, data=None, prev=None, next=None):
         self.data = data
         self.prev = prev
         self.next = next
@@ -15,7 +15,7 @@ class New_Node:
 
 class DoubleLinkedList:
     def __init__(self):
-        self.head = none
+        self.head = None
 
     def __repr__(self):
         nodes = []
@@ -30,3 +30,34 @@ class DoubleLinkedList:
         if self.head:
             self.head.prev = new_head
         self.head = new_head
+
+    def add_to_end(self, data):
+        if not self.head:
+            self.head = New_Node(data=data)
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = New_Node(data=data, prev=curr)
+
+    def remove_from_head(self, node):
+        if node.prev:
+            node.prev.next = node.next
+        if node.next:
+            node.next.prev = node.prev
+        if node is self.head:
+            self.head = node.next
+        node.prev = None
+        node.next = None
+
+    def remove_from_end(self, node):
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.prev.next = None
+        if self.next=None:
+            New_Node.head = None
+            New_Node.prev = self.data
+
+
+
